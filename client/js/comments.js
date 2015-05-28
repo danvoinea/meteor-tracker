e8fa74615a6118ea6f07bda7df7f06a7e7c01850
Dan Voinea <git@danvoinea.ro>
2015-05-28 20:14:45 +0300

    formatting changes

--- client/js/comments.js
+++ client/js/comments.js
@@ -7,15 +7,15 @@ Meteor.subscribe("comments");
 
 Template.fullComplaint.Comments = function(){
     return Comments.find({complaint_id: this._id}, {sort: {lastUpdate: 1}}).
-        map(function(document, index){
+    map(function(document, index){
         document.index = index+1;
         //.fetch in loc de .map
-    return document;
-});
+        return document;
+    });
 }
 
 Template.fullComplaint.helpers({
-  commentSchema: function() {
-    return Schemas.Comment;
-  }
+    commentSchema: function() {
+        return Schemas.Comment;
+    }
 });
